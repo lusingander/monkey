bf3b0f1e295f03e040f627b5515369ef07203296
Kyosuke Fujimoto <kyoro.f@gmail.com>
2021-02-08 23:01:20 +0900

    If expression

--- ast/ast.go
+++ ast/ast.go
@@ -191,3 +191,48 @@ func (e *InfixExpression) String() string {
 	out.WriteString(")")
 	return out.String()
 }
+
+type IfExpression struct {
+	Token       token.Token // token.IF
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (e *IfExpression) expressionNode() {}
+
+func (e *IfExpression) TokenLiteral() string {
+	return e.Token.Literal
+}
+
+func (e *IfExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("if")
+	out.WriteString(e.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(e.Consequence.String())
+	if e.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(e.Alternative.String())
+	}
+	return out.String()
+}
+
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
+func (s *BlockStatement) statementNode() {}
+
+func (s *BlockStatement) TokenLiteral() string {
+	return s.Token.Literal
+}
+
+func (s *BlockStatement) String() string {
+	var out bytes.Buffer
+	for _, stmt := range s.Statements {
+		out.WriteString(stmt.String())
+	}
+	return out.String()
+}
